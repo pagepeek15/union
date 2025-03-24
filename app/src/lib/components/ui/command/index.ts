a98d1bc82d3e022820a4b045037748da29f85c2b
pagepeek15 <pagepeeknotice@gmail.com>
2025-03-24 11:35:43 +0800

    style(command): format component styles

--- app/src/lib/components/ui/command/index.ts
+++ app/src/lib/components/ui/command/index.ts
@@ -1,16 +1,16 @@
-import { Command as CommandPrimitive } from "cmdk-sv"
+import { Command as CommandPrimitive } from "cmdk-sv";
 
-import Root from "./command.svelte"
-import Dialog from "./command-dialog.svelte"
-import Empty from "./command-empty.svelte"
-import Group from "./command-group.svelte"
-import Item from "./command-item.svelte"
-import Input from "./command-input.svelte"
-import List from "./command-list.svelte"
-import Separator from "./command-separator.svelte"
-import Shortcut from "./command-shortcut.svelte"
+import Root from "./command.svelte";
+import Dialog from "./command-dialog.svelte";
+import Empty from "./command-empty.svelte";
+import Group from "./command-group.svelte";
+import Item from "./command-item.svelte";
+import Input from "./command-input.svelte";
+import List from "./command-list.svelte";
+import Separator from "./command-separator.svelte";
+import Shortcut from "./command-shortcut.svelte";
 
-const Loading = CommandPrimitive.Loading
+const Loading = CommandPrimitive.Loading;
 
 export {
   Root,
@@ -23,7 +23,6 @@ export {
   Separator,
   Shortcut,
   Loading,
-  //
   Root as Command,
   Dialog as CommandDialog,
   Empty as CommandEmpty,
@@ -33,5 +32,18 @@ export {
   List as CommandList,
   Separator as CommandSeparator,
   Shortcut as CommandShortcut,
-  Loading as CommandLoading
-}
+  Loading as CommandLoading,
+};
+
+export type {
+  CommandProps,
+  CommandDialogProps,
+  CommandEmptyProps,
+  CommandGroupProps,
+  CommandItemProps,
+  CommandInputProps,
+  CommandListProps,
+  CommandSeparatorProps,
+  CommandShortcutProps,
+  CommandLoadingProps,
+} from "./types";
